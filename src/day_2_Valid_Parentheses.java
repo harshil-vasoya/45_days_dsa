850940f198421d9d558ff83bc929a3e7496a93b1
harshilvasoya <harshilvasoya2812@gmail.com>
2023-07-03 20:59:02 +0530

    day 2

---
+++ src/day_2_Valid_Parentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class day_2_Valid_Parentheses {
+    public static void main(String[] args) {
+    String s="()[]{}";
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+
+    }
+}
