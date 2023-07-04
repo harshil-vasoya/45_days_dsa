b2ac7464077c19d95cbfbed502ddc42ac8b7cde9
harshilvasoya <harshilvasoya2812@gmail.com>
2023-07-04 13:42:25 +0530

    day 3

--- src/day_3_Jump_Game.java
+++ src/day_3_Jump_Game.java
@@ -1,5 +1,17 @@
 public class day_3_Jump_Game {
     public static void main(String[] args) {
-
+    int[] nums={2,3,1,1,4};
+        System.out.println(solution(nums));
+    }
+    public static boolean solution(int[] nums)
+    {
+        int reach=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(reach<i)
+                return false;
+            reach=Math.max(reach,i+nums[i]);
+        }
+        return true;
     }
 }
